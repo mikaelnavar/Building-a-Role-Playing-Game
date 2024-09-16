03bab876491c2657162eb1095662bcdcf0aba160
mikalenavar <miguenavar@hotmail.com>
2024-09-15 22:24:47 -0600

    me quede hasta win

--- script.js
+++ script.js
@@ -18,5 +18,68 @@ const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 const weapons = [
     { name: 'stick', power: 5},
-    
+    { name: 'dagger', power: 30},
+    { name: 'claw hammer', power: 50},
+    { name: 'sword', power: 100}
+];
+const monsters = [
+    {
+        name: "slime",
+        level: 2,
+        helath:15
+    },
+    {
+        name: "fanged beast",
+        level: 8,
+        helath: 60
+    },
+    {
+        name: "dragon",
+        level: 20,
+        helath: 300
+    },
+]
+const locations = [
+    {
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"],
+        "button functions": [goStore, goCave, fightDragon],
+        text: "You are in the town square. You see a sign that says \"Store\"."
+    },
+    {
+        name: "store",
+        "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"
+        ],
+        "button functions": [buyHelath, buyWeapon, goTown],
+        text: "You enter the store."
+    },
+    {
+        name: "cave",
+        "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text: " You enter the cave. You see some monsters."
+    },
+    {
+      name: "fight",
+      "button text": ["Attack", "Dodge", "Run"],
+      "button functions": [attack, dodge, goTown],
+      text: "You are fighting a monster."  
+    },
+    {
+        name: "Kill monster",
+        "button text": ["Go to town square", "Go to town square", "Go to town square"],
+        "button functions": [goTown, goTown, goTown],
+        text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+    },
+    {
+        name: "lose",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+        "button functions": [restart, restart, restart],
+        text: "You die. &#x2620;"
+    },
+    {
+        name: "win",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+        "button functions": []
+    }
 ]
